6c95335cc9f509ebffb9159ea0ee1f3c902c27f7
DavidTsyg <davidtsyg@gmail.com>
2017-07-24 12:05:00 -0700

    Update make_config.sh

--- make_config.sh
+++ make_config.sh
@@ -10,10 +10,10 @@ cat ${BASE_CONFIG} \
     <(echo -e '<ca>') \
     ${KEY_DIR}/ca.crt \
     <(echo -e '</ca>\n<cert>') \
-    ${KEY_DIR}/"${1}.crt" \
+    ${KEY_DIR}/${1}.crt \
     <(echo -e '</cert>\n<key>') \
-    ${KEY_DIR}/"${1}.key" \
+    ${KEY_DIR}/${1}.key \
     <(echo -e '</key>\n<tls-auth>') \
     ${KEY_DIR}/ta.key \
     <(echo -e '</tls-auth>') \
-    >${OUTPUT_DIR}/"${1}.ovpn"
+    >${OUTPUT_DIR}/${1}.ovpn
